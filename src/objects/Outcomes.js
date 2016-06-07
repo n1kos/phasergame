343fe6de5631eed77c7f041858c9dadefd6c5f38
nik <nik@home.hme>
2016-06-07 23:52:27 +0300

    as described now when a show is received show and hide all others or just send all others hide...

--- src/objects/Outcomes.js
+++ src/objects/Outcomes.js
@@ -10,17 +10,24 @@ export default class Outcomes extends Phaser.Sprite {
 		this.value = INIT_VALUE == undefined ? INIT_FRAME : INIT_VALUE;
 		this.scale.setTo(0.5, 0.5);
 		
-		this.myRect = makeRect(this);
 
 		this.onScoreChange = new Phaser.Signal();
 		this.onScoreChange.add(onDown, this);
+
+		Outcomes.staticCounter = Outcomes.staticCounter == undefined ? 0 : Outcomes.staticCounter;
+	    Outcomes.count = function () {
+	        return ++Outcomes.staticCounter;
+	    };
 
 		function makeRect(that) {
 			var selectionPanel = game.add.graphics(that.x, that.y);
 			selectionPanel.lineStyle(4, 0xEFF, 4);
 			selectionPanel.drawRect(-5, -5, that.width + 10, that.height + 10);
+			if (Outcomes.staticCounter != 1) {
+				selectionPanel.alpha = 0;
+			}
 			return selectionPanel;
-		}
+		}		
 
 		function onDown(sprite, pointer) {
 			// do something wonderful here
@@ -29,6 +36,9 @@ export default class Outcomes extends Phaser.Sprite {
 			// selectionPanel.lineStyle(4, 0xEFF, 4);
 			// selectionPanel.drawRect(-5, -5, this.width + 10, this.height + 10);
 		}
+
+	    Outcomes.count();
+
 
 		// Outcomes.prototype.initWind = function(){
 		// 	function calculateWind() {
@@ -39,6 +49,9 @@ export default class Outcomes extends Phaser.Sprite {
 		// 	return this;		
 		// };
 		// this.initWind();
+
+		this.myRect = makeRect(this);
+
 		return this;
 	}
 	update() {
